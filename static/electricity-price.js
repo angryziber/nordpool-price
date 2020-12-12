89eceb2062cac0f45a7f6c471b6ceeb3eeb02772
Anton Keks <anton@codeborne.com>
2020-12-13 00:36:23 +0200

    display day prices

--- static/electricity-price.js
+++ static/electricity-price.js
@@ -20,24 +20,44 @@ customElements.define('electricity-price', class extends LitElement {
     fetch('/api/prices?country=' + this.country).then(res => res.json()).then(data => this.dayPrices = data)
   }
 
+  toPerKWh = p => p >= 0 ? (p / 1000).toFixed(4) : '?'
+
   hourPrice(h = this.hour) {
     let d = this.date
     if (h > 23) {
       d = Object.keys(this.dayPrices)[0]
       h -= 24
     }
-    return ((this.dayPrices?.[d]?.[h] ?? Infinity) / 1000).toFixed(4)
+    return this.toPerKWh(this.dayPrices[d]?.[h])
   }
 
   static styles = css`
     :host {
       text-align: center;
-    }`
+    }
+    
+    ul.day-prices {
+      display: block;
+      list-style: none;
+    }
+    
+    ul.day-prices li {
+      display: inline-block;
+      margin-right: 1em;
+    }
+    
+    .now {
+      color: red;
+    }
+  `
 
   render = () => html`
     <h2>${this.country} current price</h2>
     <h1>${this.hourPrice()} <small>€/kWh</small></h1>
     <p>${this.date} ${this.hour}-${this.hour + 1} CET</p>
     <h3>Prev: ${this.hourPrice(this.hour - 1)}, Next: ${this.hourPrice(this.hour + 1)}</h3>
+    <ul class="day-prices">
+      ${this.dayPrices[this.date].map((p, h) => html`<li class="${h === this.hour ? 'now' : ''}">${this.toPerKWh(p)}</li>`)}
+    </ul>
   `
 })
