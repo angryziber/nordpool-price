9ec97e54f5ffbe28ae8601e9e8f70ebde430b899
Anton Keks <anton@codeborne.com>
2020-12-13 22:13:44 +0200

    add more countries/regions

--- static/countries.js
+++ static/countries.js
@@ -1,2 +1,2 @@
 // the numbers are NordPool pageIds
-export default {FI: 35, EE: 47, LV: 59, LT: 53}
+export default {FI: 35, EE: 47, LV: 59, LT: 53, AT: 298578, BE: 298736, DE: 299565, FR: 299568, NL: 299571}
