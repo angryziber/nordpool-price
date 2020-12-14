b91a40486c567ca4a208d47570b1e0554f7a5e6c
Anton Keks <anton@codeborne.com>
2020-12-14 21:25:38 +0200

    add some predefined usages

--- static/cost-calculator.js
+++ static/cost-calculator.js
@@ -9,8 +9,12 @@ customElements.define('cost-calculator', class extends LitElement {
 
   constructor() {
     super()
-    this.kW = 9
-    this.hours = 3
+    this.selectPredefined(0)
+  }
+
+  selectPredefined(i) {
+    this.kW = predefined[i].kW
+    this.hours = predefined[i].h
   }
 
   static styles = css`
@@ -21,9 +25,19 @@ customElements.define('cost-calculator', class extends LitElement {
   `
 
   render = () => html`
-    <select><option>Sauna</option></select>
+    <select @input=${e => this.selectPredefined(e.target.selectedIndex)}>
+      ${predefined.map(p => html`<option>${p.name}</option>`)}
+    </select>
     <input type="number" .value=${this.kW} @input=${e => this.kW = e.target.value}> kW
     <input type="number" .value=${this.hours} @input=${e => this.hours = e.target.value}> h
     = <span>${(this.kW * this.hours * this.price / 100).toFixed(2)} €</span>
   `
 })
+
+const predefined = [
+  {name: 'Sauna', kW: 9, h: 3},
+  {name: 'Heater', kW: 2.5, h: 8},
+  {name: 'Nissan Leaf', kW: 3.3, h: 7.5},
+  {name: 'Tesla Model 3', kW: 11, h: 5},
+  {name: '100W light', kW: 0.1, h: 24}
+]
