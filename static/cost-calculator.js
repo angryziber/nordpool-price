1a097ac5efc2f5c86df2bc0709a10fe3496f75cf
Anton Keks <anton@codeborne.com>
2020-12-14 21:46:32 +0200

    include grid price and tax

--- static/cost-calculator.js
+++ static/cost-calculator.js
@@ -3,12 +3,16 @@ import {LitElement, html, css} from './deps/lit-element.js'
 customElements.define('cost-calculator', class extends LitElement {
   static properties = {
     price: {type: Number},
-    kW: {attribute: false},
-    hours: {attribute: false}
+    gridPrice: {type: Number},
+    taxPercent: {type: Number},
+    kW: {type: Number},
+    hours: {type: Number}
   }
 
   constructor() {
     super()
+    this.gridPrice = 5
+    this.taxPercent = 20
     this.selectPredefined(0)
   }
 
@@ -19,8 +23,11 @@ customElements.define('cost-calculator', class extends LitElement {
 
   static styles = css`
     input {
-      margin-left: 1em;
       width: 4em;
+    }
+    
+    .field {
+      margin-left: 1em;
     }
   `
 
@@ -28,9 +35,18 @@ customElements.define('cost-calculator', class extends LitElement {
     <select @input=${e => this.selectPredefined(e.target.selectedIndex)}>
       ${predefined.map(p => html`<option>${p.name}</option>`)}
     </select>
-    <input type="number" .value=${this.kW} @input=${e => this.kW = e.target.value}> kW
-    <input type="number" .value=${this.hours} @input=${e => this.hours = e.target.value}> h
-    = <span>${(this.kW * this.hours * this.price / 100).toFixed(2)} €</span>
+    <span class="field">
+      <input type="number" .value=${this.kW} @input=${e => this.kW = e.target.value}> kW
+    </span>
+    <span class="field">
+      <input type="number" .value=${this.hours} @input=${e => this.hours = e.target.value}> h
+    </span>
+    = <strong>${(this.kW * this.hours * (this.price + this.gridPrice) * (1 + this.taxPercent / 100) / 100).toFixed(2)} €</strong>
+
+    <div style="margin-top: 2em">
+      Grid price <input type="number" .value=${this.gridPrice} @input=${e => this.gridPrice = e.target.value}> cents/kWh -
+      Tax <input type="number" .value=${this.taxPercent} @input=${e => this.taxPercent = e.target.value}> %
+    </div>
   `
 })
 
