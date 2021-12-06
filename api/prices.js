1c6a22340aa6e358739c82e461fba332ab9c650f
Anton Keks <anton@codeborne.com>
2021-12-06 22:13:10 +0200

    add support for formetted prices over 1000 €/MWh

--- api/prices.js
+++ api/prices.js
@@ -4,7 +4,7 @@ export function extractPrices(json) {
     row.Columns.forEach(col => {
       const date = col.Name.replace(/(\d{2})-(\d{2})-(\d{4})/, '$3-$2-$1')
       if (!days[date]) days[date] = []
-      days[date].push(parseFloat(col.Value.replace(',', '.')))
+      days[date].push(parseFloat(col.Value.replace(' ', '').replace(',', '.')))
     })
   })
   return days
