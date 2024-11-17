33a5f23072cb3fa488060522317692e4f397984c
Anton Keks <anton@codeborne.com>
2024-11-17 20:03:33 +0200

    fix test

--- test/prices.test.js
+++ test/prices.test.js
@@ -2,7 +2,7 @@ import {extractPrices} from '../api/prices.js'
 import elering from './elering.js'
 import assert from 'assert'
 
-assert.deepStrictEqual(extractPrices(elering, 'EE')['2024-11-16'], [
+assert.deepStrictEqual(extractPrices(elering, 'EE', 1)['2024-11-16'], [
   1.58,     0,     0, -0.01,    0,
   0.03,     0,  5.29,  5.78,  5.7,
   0,     0,  0.01,  0.34, 1.52,
